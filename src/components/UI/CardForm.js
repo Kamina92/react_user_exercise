351adc7aae732d760f013df9473e0f01e4eabc6c
Filippo Della Rupa <filippo.dellarupa@gmail.com>
2022-07-12 08:51:05 +0200

    Project starting setup done

---
+++ src/components/UI/CardForm.js
@@ -0,0 +1,16 @@
+import React from "react";
+import UserForm from '../UserForm/UserForm';
+
+import styles from './CardForm.module.css';
+
+const CardForm = (props) => {
+
+    return (
+        <div className={styles.card_form}>
+            <h2>test</h2>
+        </div>
+    );
+
+};
+
+export default CardForm;
